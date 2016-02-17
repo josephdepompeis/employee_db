9487a8c6618b458f0bdb516bbd08052520db62d0
Joseph DePompeis <josephdepompeis@gmail.com>
2016-02-17 17:15:04 -0500

    first commit

---
+++ code.rb
@@ -0,0 +1,18 @@
+# require './employee_and_department_migration'
+
+# EmployeeAndDepartmentMigration.migrate(:up)
+
+require './database_configuration.rb'
+require './employee.rb'
+require './department.rb'
+
+e = Employee.create(name: "Wombles", email: "wombly@gmail.com", phone: "123-324-5234", salary: 50000)
+p e
+
+f = Employee.new(name: "Witney", email: "Wilko@gmail.com", phone: "444-333-5555", salary: 60000)
+f.save
+
+g = Department.new(name: "Department Of Love")
+g.save
+
+p = Department.where(name:"Department Of Love")
---
+++ employee_and_department_migration.rb
@@ -0,0 +1,23 @@
+require './database_configuration'
+
+
+class EmployeeAndDepartmentMigration < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.references :department
+      t.string :email
+      t.string :phone
+      t.integer :salary
+      t.string :review
+      t.boolean :satisfactory
+      t.timestamps
+    end
+
+    create_table :departments do |t|
+      t.string :name
+      t.string :review
+      t.timestamps
+    end
+  end
+end
